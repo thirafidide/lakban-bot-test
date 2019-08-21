e84951f340c776d55de450753b3a7d4c97159bf3
iamn00b <thirafi.dide@gmail.com>
2019-08-21 22:49:25 +0700

    test

---
+++ greet-issue/src/main.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const greeting =
+  'Thanks for opening this issue! This message generated automatically';
+
+async function run() {
+  try {
+    const client: github.GitHub = new github.GitHub(
+      core.getInput('repo-token', { required: true })
+    );
+
+    const context = github.context;
+
+    if (!context.payload.issue) {
+      core.debug('Not an issue, skipping');
+    }
+
+    if (context.payload.action !== 'opened') {
+      core.debug('No issue was opened, skipping');
+      return;
+    }
+
+    await client.issues.createComment({
+      ...context.issue,
+      body: greeting,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+    return;
+  }
+}
+
+run();
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import typescript from 'rollup-plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'greet-issue/src/main.ts',
+  output: {
+    file: 'lib/main.js',
+    format: 'cjs',
+  },
+  plugins: [
+    json(),
+    typescript(),
+    resolve(), // tells Rollup how to find date-fns in node_modules
+    commonjs(), // converts date-fns to ES modules
+    production && terser(), // minify, but only in production
+  ],
+};
